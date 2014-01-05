85205cc5d90912634c24f596450a5f69d2038f81
james-dibble <james@jdibble.co.uk>
2014-01-05 19:13:28 +0000

    Adding EntityFramework and creating context.

---
+++ Ketchup.Persistence.EntityFramework/KetchupContext.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="KetchupContext.cs" company="James Dibble">
+//    Copyright 2012 James Dibble
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Ketchup.Persistence.EntityFramework
+{
+    using System.Data.Entity;
+
+    using Ketchup.Model;
+
+    /// <summary>
+    /// An EntityFramework context for use with Ketchup.
+    /// </summary>
+    public class KetchupContext : DbContext
+    {
+        /// <summary>
+        /// Gets or sets the <see cref="Image"/> <see cref="DbSet"/>.
+        /// </summary>
+        public IDbSet<Image> Images { get; set; }
+
+        /// <summary>
+        /// Gets or sets the <see cref="Product"/> <see cref="DbSet"/>.
+        /// </summary>
+        public IDbSet<Product> Products { get; set; }
+        /// <summary>
+        /// Gets or sets the <see cref="ProductAttribute"/> <see cref="DbSet"/>.
+        /// </summary>
+        public IDbSet<ProductAttribute> ProductAttributes { get; set; }
+        /// <summary>
+        /// Gets or sets the <see cref="ProductAttributeType"/> <see cref="DbSet"/>.
+        /// </summary>
+        public IDbSet<ProductAttributeType> ProductAttributeTypes { get; set; }
+        /// <summary>
+        /// Gets or sets the <see cref="ProductCategory"/> <see cref="DbSet"/>.
+        /// </summary>
+        public IDbSet<ProductCategory> ProductCategorys { get; set; }
+        /// <summary>
+        /// Gets or sets the <see cref="ProductCategorySpecification"/> <see cref="DbSet"/>.
+        /// </summary>
+        public IDbSet<ProductCategorySpecification> ProductCategorySpecifications { get; set; }
+        /// <summary>
+        /// Gets or sets the <see cref="ProductSpecification"/> <see cref="DbSet"/>.
+        /// </summary>
+        public IDbSet<ProductSpecification> ProductSpecifications { get; set; }
+    }
+}
