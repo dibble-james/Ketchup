e84ac0390dc965bfbea09f6481e35cb275c1c516
james-dibble <james@jdibble.co.uk>
2014-01-07 19:12:43 +0000

    Increasing retrieval methods for products.

--- Ketchup.Api/IProductManager.cs
+++ Ketchup.Api/IProductManager.cs
@@ -5,6 +5,8 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace Ketchup.Api
 {
+    using System.Collections.Generic;
+
     using Ketchup.Model;
 
     /// <summary>
@@ -38,5 +40,56 @@ namespace Ketchup.Api
         /// <param name="validationRegularExpression">A regular expression to validate the value of a <see cref="ProductAttribute"/>.</param>
         /// <returns>The new <see cref="ProductAttributeType"/>.</returns>
         ProductAttributeType CreateAttributeType(string name, string displayName, string validationRegularExpression);
+
+        /// <summary>
+        /// Retrieve all known <see cref="ProductAttributeType"/>s.
+        /// </summary>
+        /// <returns>All known <see cref="ProductAttributeType"/>s.</returns>
+        IEnumerable<ProductAttributeType> GetProductAttributeTypes();
+
+        /// <summary>
+        /// Retrieve a <see cref="ProductAttributeType"/> by name.
+        /// </summary>
+        /// <param name="name">The name of the <see cref="ProductAttributeType"/> required.</param>
+        /// <returns>
+        /// A <see cref="ProductAttributeType"/> or null if no single <see cref="ProductAttributeType"/> exists.
+        /// </returns>
+        ProductAttributeType GetProductAttributeType(string name);
+
+        /// <summary>
+        /// Retrieve all known <see cref="Product"/>s.
+        /// </summary>
+        /// <returns>All known <see cref="Product"/>s.</returns>
+        IEnumerable<Product> GetProducts();
+
+        /// <summary>
+        /// Retrieve all known <see cref="ProductCategory"/>s.
+        /// </summary>
+        /// <returns>All known <see cref="ProductCategory"/>s.</returns>
+        IEnumerable<ProductCategory> GetProductCategories();
+
+        /// <summary>
+        /// Retrieve the child <see cref="ProductCategory"/>s for a given <paramref name="parentCategory"/>.
+        /// </summary>
+        /// <returns>The child <see cref="ProductCategory"/>s</returns>
+        IEnumerable<ProductCategory> GetProductCategories(ProductCategory parentCategory);
+
+        /// <summary>
+        /// Retrieve a <see cref="ProductCategory"/> by name.
+        /// </summary>
+        /// <param name="name">The name of the <see cref="ProductCategory"/> required.</param>
+        /// <returns>
+        /// A <see cref="ProductCategory"/> or null if no single <see cref="ProductCategory"/> exists.
+        /// </returns>
+        ProductCategory GetProductCategory(string name);
+
+        /// <summary>
+        /// Retrieve a <see cref="ProductCategory"/> by ID.
+        /// </summary>
+        /// <param name="id">The ID of the <see cref="ProductCategory"/> required.</param>
+        /// <returns>
+        /// A <see cref="ProductCategory"/> or null if no single <see cref="ProductCategory"/> exists.
+        /// </returns>
+        ProductCategory GetProductCategory(int id);
     }
 }
--- Ketchup.Api/ProductManager.cs
+++ Ketchup.Api/ProductManager.cs
@@ -6,6 +6,7 @@
 namespace Ketchup.Api
 {
     using System;
+    using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Globalization;
     using System.Text.RegularExpressions;
@@ -116,6 +117,96 @@ namespace Ketchup.Api
             this._persistence.Commit();
 
             return productAttributeType;
+        }
+
+        /// <summary>
+        /// Retrieve all known <see cref="ProductAttributeType"/>s.
+        /// </summary>
+        /// <returns>All known <see cref="ProductAttributeType"/>s.</returns>
+        public IEnumerable<ProductAttributeType> GetProductAttributeTypes()
+        {
+            var attributeTypes = this._persistence.Find(new PersistenceCollectionSearcher<ProductAttributeType>());
+
+            return attributeTypes;
+        }
+
+        /// <summary>
+        /// Retrieve a <see cref="ProductAttributeType"/> by name.
+        /// </summary>
+        /// <param name="name">The name of the <see cref="ProductAttributeType"/> required.</param>
+        /// <returns>
+        /// A <see cref="ProductAttributeType"/> or null if no single <see cref="ProductAttributeType"/> exists.
+        /// </returns>
+        public ProductAttributeType GetProductAttributeType(string name)
+        {
+            var attribute =
+                this._persistence.Find(new PersistenceSearcher<ProductAttributeType>(pat => pat.Name == name));
+
+            return attribute;
+        }
+
+        /// <summary>
+        /// Retrieve all known <see cref="Product"/>s.
+        /// </summary>
+        /// <returns>All known <see cref="Product"/>s.</returns>
+        public IEnumerable<Product> GetProducts()
+        {
+            var products = this._persistence.Find(new PersistenceCollectionSearcher<Product>());
+
+            return products;
+        }
+
+        /// <summary>
+        /// Retrieve all known <see cref="ProductCategory"/>s.
+        /// </summary>
+        /// <returns>All known <see cref="ProductCategory"/>s.</returns>
+        public IEnumerable<ProductCategory> GetProductCategories()
+        {
+            var categories = this._persistence.Find(new PersistenceCollectionSearcher<ProductCategory>());
+
+            return categories;
+        }
+
+        /// <summary>
+        /// Retrieve the child <see cref="ProductCategory"/>s for a given <paramref name="parentCategory"/>.
+        /// </summary>
+        /// <returns>The child <see cref="ProductCategory"/>s</returns>
+        public IEnumerable<ProductCategory> GetProductCategories(ProductCategory parentCategory)
+        {
+            var categories = this._persistence.Find(new PersistenceCollectionSearcher<ProductCategory>(
+                pc => pc.ParentCategory.Id == parentCategory.Id));
+
+            return categories;
+        }
+
+        /// <summary>
+        /// Retrieve a <see cref="ProductCategory"/> by name.
+        /// </summary>
+        /// <param name="name">The name of the <see cref="ProductCategory"/> required.</param>
+        /// <returns>
+        /// A <see cref="ProductCategory"/> or null if no single <see cref="ProductCategory"/> exists.
+        /// </returns>
+        public ProductCategory GetProductCategory(string name)
+        {
+            var category =
+                this._persistence.Find(new PersistenceSearcher<ProductCategory>(pat => pat.Name == name));
+
+            return category;
+        }
+
+        /// <summary>
+        /// Retrieve a <see cref="ProductCategory"/> by ID.
+        /// </summary>
+        /// <param name="id">The ID of the <see cref="ProductCategory"/> required.</param>
+        /// <returns>
+        /// A <see cref="ProductCategory"/> or null if no single <see cref="ProductCategory"/> exists.
+        /// </returns>
+        public ProductCategory GetProductCategory(int id)
+        {
+            var category =
+                this._persistence.Find(new PersistenceSearcher<ProductCategory>(pat => pat.Id == id));
+
+            return category;
         }
     }
 }
