01e40fceff3528a93b2b6cfbe671a7c85495cd82
james-dibble <james@jdibble.co.uk>
2014-01-16 15:30:12 +0000

    Adding an awesome solution for seeding EF contexts.

---
+++ Ketchup.Persistence.EntityFramework/IKetchupSeeder.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IKetchupSeeder.cs" company="James Dibble">
+//    Copyright 2012 James Dibble
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Ketchup.Persistence.EntityFramework
+{
+    using System;
+    using System.Collections.Generic;
+
+    public interface IKetchupSeeder
+    {
+        IEnumerable<Action<KetchupContext>> SeedMethods { get; } 
+    }
+}
